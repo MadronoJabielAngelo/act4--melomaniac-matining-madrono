de9900fdcc3094bb9586291289e63f1795a48d09
MadronoJabiel <jabsmadrono@gmail.com>
2024-03-09 01:40:14 +0800

    reediting the web ui

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body style="background-color: #000000;">
+<body style="background-color: black;">
     <h1>Activity #5</h1>
     <a> </a>
     <a>Mark John B. Matinig & Jabiel Angelo C. Madrono | BSI/T-2A</a>
